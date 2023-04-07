328c639681a497d9f76d09690b1288ddb39cd963
siddhant-afk <siddhantdudhane21@gmail.com>
2023-04-07 10:47:03 +0530

    Create trigger_report endpoint

--- server.py
+++ server.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask,jsonify
 import os
 from dotenv import load_dotenv
 import pandas as pd
 import psycopg2
-
+import string
+import random
+import uuid
 load_dotenv()
 
 app = Flask(__name__)
@@ -15,24 +17,56 @@ username  =os.getenv("USER")
 password = os.getenv("PWD")
 port = os.getenv("PORT_ID")
 
-try:
+
+
+@app.route("/trigger_report")
+def trigger_report():
+    report_id = ''.join(random.choices(string.ascii_uppercase + string.digits, k=16))
+
+
     connection = psycopg2.connect(host = host,
                             dbname = database,
                             user = username,
                             password = password,
                             port = port)
-except Exception as error:
-    print(error)
+    
+    cursor = connection.cursor()
+
+    try:
+
+        cursor.execute(f"INSERT INTO Report VALUES('{report_id}','Running')")
+        connection.commit()
+
+        # TODO : Generate report asyncrhonously
+        
+        return jsonify({"Report ID" : report_id })
+    except:
+        connection.rollback()
+        raise
+
+    finally:
+        cursor.close()
+        connection.close()
 
 
-@app.route("/api/data/store_data")
-def fetch_data():
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT * FROM store_data")
-            data = cursor.fetchall()[:50]
-            return {"data" : data}
+
+
+
+
+
+    
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+    # with connection:
+    #     with connection.cursor() as cursor:
+    #         cursor.execute(f"SELECT * FROM store_data WHERE store_id = {store_id}")
+    #         data = cursor.fetchall()
+
+    #         return {"length" : len(data),"data" : data}
